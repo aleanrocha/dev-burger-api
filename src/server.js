8617cb88607b01899a7e8145007ecbc596338ebe
aleanrocha <alean.oficial.me@gmail.com>
2024-04-20 22:57:31 -0300

    feat: add Nodemon and Sucrase for enhanced development workflow and ES module support

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const app = require('./app')
+import app from './app'
 
 const port = 3001
 
-app.listen(port, () => console.log(`Server is running at port ${port}.`))
+app.listen(port, () => console.log(`Server is running at port ${port} 🚀🚀🚀`))
