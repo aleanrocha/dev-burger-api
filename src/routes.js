f7ac0487959802d31ec909470b616d5edc00fc25
aleanrocha <alean.oficial.me@gmail.com>
2024-04-22 17:44:57 -0300

    feat: create user controller

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,12 @@
 import { Router } from 'express'
-
-import { v4 } from 'uuid'
-
-import User from './app/models/User'
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
 
-routes.get('/', async (_, res) => {
-  // return res.status(200).json({ message: 'OK' })
-  const user = await User.create({
-    id: v4(),
-    name: 'Alean',
-    email: 'alean@gmail.com',
-    password_hash: 'enfqnbfqkbefçcoQANQcvçoAQ',
-  })
-  return res.status(201).json(user)
+routes.get('/', (_, res) => {
+  return res.status(200).json({ message: 'OK' })
 })
+
+routes.post('/users', UserController.store)
 
 export default routes
