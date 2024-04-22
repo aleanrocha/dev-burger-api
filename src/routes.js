a6203c106d9f13e2ad914b862b3d803f4b62a846
aleanrocha <alean.oficial.me@gmail.com>
2024-04-21 23:53:27 -0300

    feat: connect user model to database

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express'
 
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
+
 const routes = new Router()
 
-routes.get('/', (_, res) => {
-  return res.status(200).json({ message: 'OK' })
+routes.get('/', async (_, res) => {
+  // return res.status(200).json({ message: 'OK' })
+  const user = await User.create({
+    id: v4(),
+    name: 'Alean',
+    email: 'alean@gmail.com',
+    password_hash: 'enfqnbfqkbefçcoQANQcvçoAQ',
+  })
+  return res.status(201).json(user)
 })
 
 export default routes
