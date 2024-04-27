e7be39fc7bccc5efa68d156c0535bbfcf602969b
aleanrocha <alean.oficial.me@gmail.com>
2024-04-26 22:22:28 -0300

    feat: save orders in MongoDB

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup'
 
+import Order from '../schemas/Order'
 import Product from '../models/Product'
 import Category from '../models/Category'
 
@@ -55,9 +56,12 @@ class OrderController {
         name: req.userName,
       },
       products: formatedProducts,
+      status: 'Pedido realizado',
     }
 
-    return res.status(201).json(order)
+    const createdOrder = await Order.create(order)
+
+    return res.status(201).json(createdOrder)
   }
 }
 
