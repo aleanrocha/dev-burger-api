0c71eb4dfead29c5d07b399326314afb62270058
aleanrocha <alean.oficial.me@gmail.com>
2024-04-27 01:54:03 -0300

    feat: update categories

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -35,6 +35,58 @@ class CategoryController {
     return res.status(201).json({ id: category.id, name })
   }
 
+  async update(req, res) {
+    const categorySchema = Yup.object({
+      name: Yup.string(),
+    })
+
+    try {
+      categorySchema.validateSync(req.body, { abortEarly: false })
+    } catch (error) {
+      return res.status(400).json({ error: error.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(req.userId)
+
+    if (!isAdmin) {
+      return res.status(401).json()
+    }
+
+    const { id } = req.params
+    const findCategory = await Category.findByPk(id)
+
+    if (!findCategory) {
+      return res
+        .status(400)
+        .json({ error: 'Make sure ID product are correct.' })
+    }
+
+    let path
+
+    if (req.file) {
+      path = req.file.filename
+    }
+
+    const { name } = req.body
+
+    if (name) {
+      const categoryExists = await Category.findOne({ where: { name } })
+
+      if (categoryExists && categoryExists.id !== +id) {
+        return res.status(400).json({ error: 'Category already exists!' })
+      }
+    }
+
+    await Category.update(
+      { name, path },
+      {
+        where: { id },
+      },
+    )
+
+    return res.status(200).json()
+  }
+
   async index(_, res) {
     const categories = await Category.findAll()
     return res.status(200).json(categories)
--- src/routes.js
+++ src/routes.js
@@ -30,7 +30,7 @@ routes.put('/products/:id', upload.single('file'), ProductController.update)
 
 routes.get('/categories', CategoryController.index)
 routes.post('/categories', upload.single('file'), CategoryController.store)
-// routes.put('/categories/:id', CategoryController.update)
+routes.put('/categories/:id', upload.single('file'), CategoryController.update)
 
 routes.get('/orders', OrderController.index)
 routes.post('/orders', OrderController.store)
